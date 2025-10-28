e8fc252d7da61b9b167ee3bb7aff1525b1fb386b
alnsimao <alansimao9a1@gmail.com>
2025-10-28 17:36:54 -0300

    first commit

---
+++ src/main/java/com/aln/project/AgendeMogiApplication.java
@@ -0,0 +1,13 @@
+package com.aln.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendeMogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendeMogiApplication.class, args);
+	}
+
+}
